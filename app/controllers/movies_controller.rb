4bed160f6a08546221a2b2cfb3dabdd84067d87e
vayaji <176355251+vayaji@users.noreply.github.com>
2025-06-21 20:36:21 +0900

    station06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,16 @@
 class MoviesController < ApplicationController
   def index
+    keyword = params[:keyword]
+    is_showing = params[:is_showing]
     @movies = Movie.all
+    if is_showing == "1"
+      @movies = @movies.where(is_showing: true)
+    elsif is_showing == "0"
+      @movies = @movies.where(is_showing: false)
+    end
+
+    if keyword.present?
+      @movies = @movies.where("name LIKE ? OR description LIKE ?", "%#{keyword}%", "%#{keyword}%")
+    end
   end
 end
